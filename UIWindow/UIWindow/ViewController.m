998d41c88320792eaab879151cdd4b78bc15e9a8
ericsonyc <1220246394@qq.com>
2015-12-25 09:42:33 +0800

    add uiwindow project

---
+++ UIWindow/UIWindow/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIWindow
+//
+//  Created by ericson on 15/12/25.
+//  Copyright © 2015年 ericson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ UIWindow/UIWindow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UIWindow
+//
+//  Created by ericson on 15/12/25.
+//  Copyright © 2015年 ericson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
