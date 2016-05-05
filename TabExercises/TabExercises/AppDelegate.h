32f051f560f1751ffa1cee425d1ec7a6eb24c919
ericsonyc <1220246394@qq.com>
2016-05-06 01:09:25 +0800

    add projects

---
+++ TabExercises/TabExercises/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabExercises
+//
+//  Created by ericson on 16/5/6.
+//  Copyright © 2016年 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabExercises/TabExercises/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  TabExercises
+//
+//  Created by ericson on 16/5/6.
+//  Copyright © 2016年 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
